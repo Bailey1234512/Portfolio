01a5e29e3725a284511061cfa04c9a90f59d6d84
Bailey1234512 <jbgc87@gmail.com>
2023-02-27 09:57:24 +0000

    added net-matters clone to coding examples

--- js/validation.js
+++ js/validation.js
@@ -5,7 +5,6 @@ function validateName() {
   if (nameInput.value === '') {
     nameInput.style.borderColor = 'red';
     nameError.textContent = 'Please enter your name';
-    console.log('Please enter your name');
     return false;
   }
   nameError.textContent = '';
@@ -18,18 +17,17 @@ nameInput.addEventListener('blur', validateName);
 
 const emailInput = document.getElementById('email');
 const emailError = document.getElementById('emailError');
-const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const emailRegex = /^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$/;
+
 
 function validateEmail() {
     if (emailInput.value === '') {
       emailInput.style.borderColor = 'red';
       emailError.textContent = 'Please enter your email';
-      console.log('Please enter your email');
       return false;
     } else if (!emailRegex.test(emailInput.value)) {
       emailError.textContent = 'Please enter a valid email address';
       emailInput.style.borderColor = 'red';
-      console.log('Please enter a valid email address');
       return false;
     }
     emailError.textContent = '';
@@ -38,6 +36,66 @@ function validateEmail() {
   }
 
 emailInput.addEventListener('blur', validateEmail);
+
+
+
+
+const subjectInput = document.getElementById('subject');
+const subjectError = document.getElementById('subjectError');
+
+function validateSubject() {
+  if (subjectInput.value === '') {
+    subjectInput.style.borderColor = 'red';
+    subjectError.textContent = 'Please enter your subject';
+    return false;
+  }
+  subjectError.textContent = '';
+  subjectInput.style.borderColor = '';
+  return true;
+}
+
+subjectInput.addEventListener('blur', validateSubject);
+
+
+
+
+const messageInput = document.getElementById('message');
+const messageError = document.getElementById('messageError');
+
+function validateMessage() {
+  if (messageInput.value === '') {
+    messageInput.style.borderColor = 'red';
+    messageError.textContent = 'Please enter your message';
+    return false;
+  }
+  messageError.textContent = '';
+  messageInput.style.borderColor = '';
+  return true;
+}
+
+messageInput.addEventListener('blur', validateMessage);
+
+
+
+const contactForm = document.getElementById('contact-form');
+const sendBtn = document.querySelector('.btn');
+
+sendBtn.addEventListener('click', function() {
+  validateForm();
+});
+
+function validateForm() {
+  let isValid = true;
+  if (!validateName()) isValid = false;
+  if (!validateEmail()) isValid = false;
+  if (!validateSubject()) isValid = false;
+  if (!validateMessage()) isValid = false;
+  if (isValid) {
+    form.submit();
+  }
+}
+
+
 
 
 const form = document.getElementById('contact-form');
